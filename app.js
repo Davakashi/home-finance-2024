5205e69858caff7aa5141035d6a1974e075d08d4
Davakashi <daavka.desu@gmail.com>
2024-01-14 17:18:54 +0800

    code oo tsegtselj sanhuugiin moduliin objectiig uusgene

--- app.js
+++ app.js
@@ -1,22 +1,63 @@
 // Дэлгэцтэй ажиллах модуль
-var uiController = (function () {})();
+var uiController = (function () {
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDesciption: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn",
+    };
+
+    return {
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDesciption)
+                    .value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+        getDOMStrings: function () {
+            return DOMstrings;
+        },
+    };
+})();
 
 // Санхүүтэй ажиллах модуль
 var financeController = (function () {})();
 
 // Програмын холбоостой ажиллах модуль
 var appController = (function (uiController, financeController) {
-    document.querySelector(".add__btn").addEventListener("click", function () {
+    var ctrlAddItem = function () {
         // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+        console.log(uiController.getInput());
+
         // 2. Олж авсан өгөгдлүүдээ санхүүгийн модули руу дамжуулж хадгална.
         // 3. Олж авсан өгөгдлүүдээ вэб дээр тохирох хэсэг тавьна.
         // 4. Төсвийг тооцоолно.
         // 5. Эцсийн үлдэгдэл тооцооог дэлгэцэнд гаргана.
-    });
+    };
 
-    document.addEventListener("keypress", function (event) {
-        if (event.keyCode === 13) {
-            console.log("ajilttai");
-        }
-    });
+    var setupEventListeners = function () {
+        var DOM = uiController.getDOMStrings();
+        document
+            .querySelector(DOM.addBtn)
+            .addEventListener("click", function () {
+                ctrlAddItem();
+            });
+
+        document.addEventListener("keypress", function (event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        });
+    };
+
+    return {
+        init: function () {
+            console.log("application started...");
+            setupEventListeners();
+        },
+    };
 })(uiController, financeController);
+
+appController.init();
